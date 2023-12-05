efd9f2934fd356f8ae161cc05d440ec9c3fb542f
Pranav Mathur <pranavmathur2.0@gmail.com>
2023-12-05 19:50:00 +0530

    Initial Commit

---
+++ profile-mini-app/profile_mini_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  profile_mini_appApp.swift
+//  profile-mini-app
+//
+//  Created by Pranav Mathur on 05/12/23.
+//
+
+import SwiftUI
+
+@main
+struct profile_mini_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
